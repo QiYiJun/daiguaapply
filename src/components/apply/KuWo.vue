3dae3e0446b7b4419526e6fe7a9347e27bd7690e
QiYiJun <1497055242@qq.com>
2023-09-12 16:23:19 +0800

    update kuwo music page

--- src/components/apply/KuWo.vue
+++ src/components/apply/KuWo.vue
@@ -8,16 +8,14 @@
     <el-card>
       <div class='tips'>酷我音乐</div>
       <div class="teach">
-        <span>"我的"-"做任务赚钱"</span><br>
-        <span>
-          <img src="../../assets/img/kuwo.png">
-        </span><br>
-        <span>抓取链接示例: "https://integralapi.kuwo.cn/api/v1/online/sign/v1/earningSignIn/everydaymusic/doListen?xxxxxxxxxxxxxxxxxxx"</span>
+        <el-button type="primary"
+                   @click="teachDialogVisible = true">查看教程</el-button>
       </div>
+
       <el-form :model="KuWoForm"
                :rules="KuWoRules"
                ref="KuWoRef"
-               label-width="70px">
+               label-width="128px">
         <el-form-item label="Ids"
                       prop="ids">
           <el-input v-model="KuWoForm.ids"
@@ -34,6 +32,42 @@
         </el-form-item>
       </el-form>
     </el-card>
+
+    <el-dialog title="教程"
+               :visible.sync="teachDialogVisible"
+               width="50%">
+      <span class="title">
+        酷我音乐任务入口:
+      </span>
+      <span>请点击下方的"任务入口"按钮查看详情</span>
+      <span class="title">
+        Ids和Cookies的获得:
+      </span>
+      <span>抓取链接: "https://integralapi.kuwo.cn/api/v1/online/sign/new/boxRenew?xxxxxx"</span>
+      <span>Ids在链接中, Cookies在请求头里的, 复制完整的cookies即可</span>
+
+      <el-dialog title="任务入口"
+                 :visible.sync="imgDialogVisible"
+                 width="50%"
+                 append-to-body>
+        <div class="apply">
+          <span>"我的"-"做任务赚钱"</span>
+          <img src="../../assets/img/kuwo.png">
+        </div>
+        <span slot="footer"
+              class="dialog-footer">
+          <el-button type="primary"
+                     @click="imgDialogVisible = false">关闭</el-button>
+        </span>
+
+      </el-dialog><span slot="footer"
+            class="dialog-footer">
+        <el-button type="success"
+                   @click="imgDialogVisible = true">任务入口</el-button>
+        <el-button type="primary"
+                   @click="teachDialogVisible = false">关 闭</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -42,6 +76,8 @@ export default {
   name: 'KuWo',
   data () {
     return {
+      teachDialogVisible: false,
+      imgDialogVisible: false,
       KuWoForm: {
         ids: '',
         cookies: ''
@@ -84,7 +120,21 @@ export default {
   margin-bottom: 25px;
 }
 
-span img {
+.title {
+  font-weight: bold;
+  font-size: 20px;
+}
+
+span {
+  padding: 5px 0;
+  display: block;
+}
+
+.apply {
+  text-align: center;
+}
+
+.apply img {
   width: 200px;
 }
 </style>
